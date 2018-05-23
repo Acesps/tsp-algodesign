87619250e616b9a7fea1bd725836fba0b3a0bdb2
Acesps <000sps97@gmail.com>
2018-05-24 00:03:15 +0530

    completed finally :')(testing left)

--- tsp-algo-design.py
+++ tsp-algo-design.py
@@ -26,7 +26,6 @@ def travllingSalesmanProblem(G,n,nodes):
     min_path = n*1000;
     ans = []
     while(len(nodes) > 0):
- 
         # store current Path weight(cost)
         current_pathweight = 0;
          
@@ -36,12 +35,20 @@ def travllingSalesmanProblem(G,n,nodes):
             current_pathweight += G.edge[nodes[i]][nodes[i+1]]['cost'];
         current_pathweight += G.edge[nodes[-1]][1]['cost'];
  
+        if(min_path == current_pathweight):
+        	ans.append(copy.deepcopy(nodes))
+        	#print "smaller value found at"
+        	#print nodes
         #update minimum
         if(min_path > current_pathweight):
+        	ans = []
         	min_path = current_pathweight
-        	ans = copy.deepcopy(nodes)
+        	ans.append(copy.deepcopy(nodes))
+        	#print "smaller value found at"
+        	#print nodes 
         nodes = next_permutation(nodes)
-    return min_path,ans;
+    print ans;
+    return min_path,ans[0];
 
  
 def visualize(G,graph_pos,node_color='red', node_alpha=0.3,
@@ -55,53 +62,154 @@ def visualize(G,graph_pos,node_color='red', node_alpha=0.3,
 
 def tryit(G,n,nodes):
 	#initializeing for base case 
-	ans=[1,2,3,1]
+	list_ans=[[1,2,3,1]]
+	list_newans = []
 	distance = G.edge[1][2]['cost'] + G.edge[2][3]['cost'] + G.edge[3][1]['cost']
 	for i in range(4,len(nodes)+2):
 		minimum_triangle = n*1000 #cost of adding 
-		# for j in range(0,len(ans)-1):
-		# 	triangle = -G.edge[ans[j]][ans[j+1]]['cost'] + G.edge[i][ans[j+1]]['cost'] + G.edge[i][ans[j]]['cost'];
-		# 	if(triangle < minimum_triangle):
-		# 		minimum_triangle = triangle
-		# 		k = j 
-		# distance += minimum_triangle
-		# ans.insert(k+1,i)
-		newans = ans
-		for i1 in range(0,len(ans)-1):
-			for i2 in range(1,len(ans)):
-				if i1 == i2 :
-					continue
-				if(i1 + 1 = i2) or (i1 = i2 + 1 ):
-					triangle = -G.edge[ans[i1]][ans[i2]]['cost'] + G.edge[i][ans[i2]]['cost'] + G.edge[i][ans[i1]]['cost'];
-				 	if(triangle < minimum_triangle):
-				 		minimum_triangle = triangle
-				 		newans = ans
-				 		newans.insert(max(i2,i1),i)
-				elif i1 < i2:
-					cost = G.edge[ans[i1]][i] + G.edge[ans[i2]][i] - G.edge[ans[i1]][ans[i1+1]] -G.edge[ans[i2]][ans[i2-1]];
-					if cost > minimum_triangle:
+		list_newans = copy.deepcopy(list_ans)
+		for ans in list_ans:
+			# for j in range(0,len(ans)-1):
+			# 	triangle = -G.edge[ans[j]][ans[j+1]]['cost'] + G.edge[i][ans[j+1]]['cost'] + G.edge[i][ans[j]]['cost'];
+			# 	if(triangle < minimum_triangle):
+			# 		minimum_triangle = triangle
+			# 		k = j 
+			# distance += minimum_triangle
+			# ans.insert(k+1,i)
+			newans = copy.deepcopy(ans)
+			for i1 in range(0,len(ans)-1):
+				for i2 in range(i1+1,len(ans)):
+					if ans[i1] == ans[i2]: #case 1 = 1 in [1,2,3,1]
 						continue
-					tempans=ans[:i1+1] + [i] + ans[i2:] #[1,2,3,..i1] + [i] + [i2,...,1]
+					if(i1 + 1 == i2):
+						triangle = -G.edge[ans[i1]][ans[i2]]['cost'] + G.edge[i][ans[i2]]['cost'] + G.edge[i][ans[i1]]['cost'];
+					 	if(triangle == minimum_triangle):
+					 		newans = copy.deepcopy(ans)
+					 		newans.insert(i2,i)
+					 		if(newans<newans[::-1]):
+					 			list_newans.append(copy.deepcopy(newans)) 
+					 		else:
+					 			list_newans.append(copy.deepcopy(newans[::-1]))
+					 		#print "running triangle"
+					 		#print ans,list_newans
 
-					for j in range(0,len(tempans)-1):
-						triangle = -G.edge[tempans[j]][tempans[j+1]]['cost'] + G.edge[ans[i1]][tempans[j]]['cost'] + G.edge[ans[i2]][tempans[j+1]]['cost'];
-				 		if(cost + triangle) < minimum_triangle:
-				 			minimum_triangle = cost + triangle;
-				 			newans = tempans[:j+1] + ans[i1+1:i2] + temp[j+1:]
-						triangle = -G.edge[tempans[j]][tempans[j+1]]['cost'] + G.edge[ans[i1]][tempans[j+1]]['cost'] + G.edge[ans[i2]][tempans[j]]['cost'];
-				 		if(cost + triangle) < minimum_triangle:
-				 			minimum_triangle = cost + triangle;
-				 			newans = tempans[:j+1] + ans[i2-1:i1:-1] + tempans[j+1:]
-				else:
-					ans.pop()
-					cost = G.edge[ans[i1]][i] + G.edge[ans[i2]][i] -G.edge[ans[i1]][i1-1] -G.edge[ans[i2]][(i2+1)%len(ans)]
-					if cost > minimum_triangle:
-						continue
-					tempans = ans[i1:i2+1] 
-					ans+=[1]
+					 	if(triangle < minimum_triangle):
+					 		list_newans = []
+					 		minimum_triangle = triangle
+					 		newans = copy.deepcopy(ans)
+					 		newans.insert(i2,i)
+					 		if(newans<newans[::-1]):
+					 			list_newans.append(copy.deepcopy(newans)) 
+					 		else:
+					 			list_newans.append(copy.deepcopy(newans[::-1]))
+					 		#print "running triangle"
+					 		#print ans,list_newans
+					else:
+						cost = G.edge[ans[i1]][i]['cost'] + G.edge[ans[i2]][i]['cost'] - G.edge[ans[i1]][ans[i1+1]]['cost'] -G.edge[ans[i2]][ans[i2-1]]['cost'];
+						if cost > minimum_triangle:
+							continue
+						tempans=ans[:i1+1] + [i] + ans[i2:] #[1,2,3,..i1] + [i] + [i2,...,1]
 
-		ans = newans
-	return distance, ans 
+						for j in range(0,len(tempans)-1):
+							triangle = -G.edge[tempans[j]][tempans[j+1]]['cost'] + G.edge[ans[i1+1]][tempans[j]]['cost'] + G.edge[ans[i2-1]][tempans[j+1]]['cost'];
+					 		if(cost + triangle) == minimum_triangle:
+					 			newans = tempans[:j+1] + ans[i1+1:i2] + tempans[j+1:]
+					 			if(newans<newans[::-1]):
+					 				list_newans.append(copy.deepcopy(newans)) 
+					 			else:
+					 				list_newans.append(copy.deepcopy(newans[::-1]))
+					 			#print "running triangle 2"
+					 			#print i1,i2,tempans,ans,list_newans
+					 		if(cost + triangle) < minimum_triangle:
+					 			list_newans = []
+					 			minimum_triangle = cost + triangle;
+					 			newans = tempans[:j+1] + ans[i1+1:i2] + tempans[j+1:]
+					 			if(newans<newans[::-1]):
+					 				list_newans.append(copy.deepcopy(newans)) 
+					 			else:
+					 				list_newans.append(copy.deepcopy(newans[::-1]))
+					 			#print "running triangle 2"
+					 			#print i1,i2,tempans,ans,list_newans
+							
+							triangle = -G.edge[tempans[j]][tempans[j+1]]['cost'] + G.edge[ans[i1+1]][tempans[j+1]]['cost'] + G.edge[ans[i2-1]][tempans[j]]['cost'];
+					 		if(cost + triangle) == minimum_triangle:
+					 			newans = tempans[:j+1] + ans[i2-1:i1:-1] + tempans[j+1:]
+								if(newans<newans[::-1]):
+						 			list_newans.append(copy.deepcopy(newans)) 
+						 		else:
+						 			list_newans.append(copy.deepcopy(newans[::-1]))
+					 			#print "running triangle 3"
+					 			#print i1,i2,tempans,ans,list_newans
+
+					 		if(cost + triangle) < minimum_triangle:
+					 			list_newans = []
+					 			minimum_triangle = cost + triangle;
+					 			newans = tempans[:j+1] + ans[i2-1:i1:-1] + tempans[j+1:]
+						 		if(newans<newans[::-1]):
+					 				list_newans.append(copy.deepcopy(newans)) 
+					 			else:
+					 				list_newans.append(copy.deepcopy(newans[::-1]))
+					 			#print "running triangle 3"
+					 			#print i1,i2,tempans,ans,list_newans
+
+						if (i1!= 0 and i2 < len(ans)-2): #if i2 = len-2 there is no point between i1 & i2
+					 		cost = G.edge[ans[i1]][i]['cost'] + G.edge[ans[i2]][i]['cost'] -G.edge[ans[i1]][ans[i1-1]]['cost'] -G.edge[ans[i2]][ans[i2+1]]['cost']
+							if cost > minimum_triangle:
+								continue
+							tempans =ans[i1:i2+1] + [i] # [i1,,,,i2] 
+							for j in range(0,len(tempans)-2):
+								triangle = -G.edge[tempans[j]][tempans[j+1]]['cost'] + G.edge[ans[i1-1]][tempans[j]]['cost'] + G.edge[ans[i2+1]][tempans[j+1]]['cost'];
+					 			if(cost + triangle) == minimum_triangle:
+					 				newans = ans[:i1] + tempans[j::-1] + tempans[:j:-1] + ans[i2+1:] 
+					 				if(newans<newans[::-1]):
+					 					list_newans.append(copy.deepcopy(newans)) 
+							 		else:
+							 			list_newans.append(copy.deepcopy(newans[::-1]))
+							 		#print "running triangle 4"
+					 				#print i1,i2,tempans,ans,list_newans
+								
+					 			if(cost + triangle) < minimum_triangle:
+					 				list_newans = []
+					 				minimum_triangle = cost + triangle;
+					 				newans = ans[:i1] + tempans[j::-1] + tempans[:j:-1] + ans[i2+1:] 
+					 				if(newans<newans[::-1]):
+							 			list_newans.append(copy.deepcopy(newans)) 
+							 		else:
+							 			list_newans.append(copy.deepcopy(newans[::-1]))
+							 		#print "running triangle 4"
+					 				#print i1,i2,tempans,ans,list_newans
+								
+								triangle = -G.edge[tempans[j]][tempans[j+1]]['cost'] + G.edge[ans[i1-1]][tempans[j+1]]['cost'] + G.edge[ans[i2+1]][tempans[j]]['cost'];
+					 			if(cost + triangle) == minimum_triangle:
+					 				newans = ans[:i1] + tempans[j+1:] + tempans[:j+1] + ans[i2+1:]
+					 				if(newans<newans[::-1]):
+							 			list_newans.append(copy.deepcopy(newans)) 
+							 		else:
+							 			list_newans.append(copy.deepcopy(newans[::-1]))
+							 		#print "running triangle 5"
+						 			#print i1,i2,tempans,ans,list_newans
+
+					 			if(cost + triangle) < minimum_triangle:
+					 				list_newans = []
+					 				minimum_triangle = cost + triangle;
+					 				newans = ans[:i1] + tempans[j+1:] + tempans[:j+1] + ans[i2+1:]
+					 				if(newans<newans[::-1]):
+							 			list_newans.append(copy.deepcopy(newans)) 
+							 		else:
+							 			list_newans.append(copy.deepcopy(newans[::-1]))
+							 		#print "running triangle 5"
+						 			#print i1,i2,tempans,ans,list_newans
+
+		distance += minimum_triangle
+		#removing duplicates
+		list_newans.sort()
+		for index in reversed(range(1,len(list_newans))):
+			if list_newans[index] == list_newans[index - 1]:
+				del list_newans[index]
+		list_ans = list_newans
+		print "adding node" + str(i) 
+		print list_ans
+	return distance, list_ans[0] 
 #enabling interactive mode in pylab
 pylab.ion()                                 
 #function to display and keep the image on screen till refreshing
@@ -115,17 +223,24 @@ start_time = time.time()
 nodes=[int(a) for a in range(2,n+1)]
 # count=0;
 # while(len(nodes)):
-# 	print nodes;
+# 	#print nodes;
 # 	nodes = next_permutation(nodes);
 # 	count+=1;
-# print "hurray"
-# print count;
+# #print "hurray"
+# #print count;
 # exit()
-# g = [ [468, 479, 421, 475, 433, 412],
-# 	[479, 428, 409, 471, 403, 465],
-# 	[421, 409, 456, 425, 492, 486],
-# 	[475, 471, 425, 404, 490, 420]],
-# 	#[433, 403, 492, 490, 481, 488] ]
+g = [[439, 478, 428, 434, 441, 464, 438, 420, 439, 448, 423, 429],
+	[478, 498, 439, 433, 467, 457, 426, 491, 491, 483, 460, 415],
+	[428, 439, 447, 479, 489, 474, 439, 483, 488, 458, 440, 450],
+	[434, 433, 479, 494, 458, 443, 474, 464, 445, 415, 403, 495],
+	[441, 467, 489, 458, 457, 463, 402, 472, 421, 427, 448, 465],
+	[464, 457, 474, 443, 463, 413, 402, 431, 426, 423, 456, 440],
+	[438, 426, 439, 474, 402, 402, 459, 455, 483, 495, 488, 472],
+	[420, 491, 483, 464, 472, 431, 455, 431, 424, 488, 406, 496],
+	[439, 491, 488, 445, 421, 426, 483, 424, 457, 416, 401, 429],
+	[448, 483, 458, 415, 427, 423, 495, 488, 416, 488, 478, 436],
+	[423, 460, 440, 403, 448, 456, 488, 406, 401, 478, 437, 444],
+	[429, 415, 450, 495, 465, 440, 472, 496, 429, 436, 444, 424]]
 
 G.add_nodes_from(nodes)
 G.add_nodes_from([1])
@@ -135,9 +250,9 @@ for i in range(1,n+1):
     	weight = G.edge[i][j]['cost']
     	weights.append(weight)
     for j in range(i,n+1):
-        weight =randint(400,500)
-        G.add_edge(i,j,{'cost' : weight })#g[i-1][j-1]})
-        weights.append(weight)#g[i-1][j-1])
+        weight = g[i-1][j-1] #randint(400,500) #
+        G.add_edge(i,j,{'cost' : weight })
+        weights.append(weight)
     print weights
 graph_pos=nx.circular_layout(G)
 visualize(G,graph_pos)
@@ -152,14 +267,13 @@ plt.clf()
 print "naive solution:"
 distance,path = travllingSalesmanProblem(G,n,nodes)
 Gtsp=nx.Graph()
-Gtsp.add_edge(1,path[0])
+path = [1] + path + [1]
 for i in range(0,len(path)-1):
 	Gtsp.add_edge(path[i],path[i+1]);
-Gtsp.add_edge(path[-1],1)
 visualize(Gtsp,graph_pos)
 pylab.draw()
 print "Done"
-print path;
+##print path;
 print distance
 print "Time of Program " + str(time.time()-start_time)
 plt.pause(5)
